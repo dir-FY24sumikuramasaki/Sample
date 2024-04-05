f521b0438334ad5ce865209906e5d7f73b08b65e
dir-FY24sumikuramasaki <MasakiSumikura()@Dirbato-1302x>
2024-04-05 15:09:23 +0900

    2-2終了

---
+++ Sample/src/sample/Test.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("kiramex");
+        System.out.println("java");
+        System.out.println("学習を始めましょう！");
+}}

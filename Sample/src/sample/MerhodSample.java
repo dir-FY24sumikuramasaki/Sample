8ae36f08fb9920b17179ef48ddf49ee063ac209b
dir-FY24sumikuramasaki <MasakiSumikura()@Dirbato-1302x>
2024-04-08 17:53:18 +0900

    復習

--- Sample/src/sample/MerhodSample.java
+++ Sample/src/sample/MerhodSample.java
@@ -1,23 +1,24 @@
 package sample;
 
+import java.security.PublicKey;
+
 public class MerhodSample {
 
     public static void main(String[] args) {
-        var number1=100;
-        var number2=10;
-        
-        sumMethod1(number1,number2);
-        
-        var result2 = sumMethod2(number1,number2);
-        System.out.println("sumMethod2の結果は"+ result2);
-        }
-
-    public static void sumMethod1(int num1,int num2) {
-        var result =num1 + num2;
-        System.out.println("sumMethod1の結果は" + result);
+       var number1 = 100;
+       var number2 = 10;
+       
+       sumMethod1(number1,number2);
+       
+       var result2 = sumMethod2(number1,number2);
+       System.out.println("sumMethod2の結果は"+ result2 +"です。");
     }
-    public static int sumMethod2(int num1,int num2) {
-        var result = num1 + num2;
-        return result;
+       public static void sumMethod1(int num1, int num2) {
+           var result1 = num1 +num2;
+           System.out.println("sumMethod1の結果は"+ result1 +"です。");
     }
+       public static int sumMethod2(int num1, int num2) {
+           var result2 = num1+num2;
+           return result2;
+       }
 }
--- Sample/src/sample/Variable.java
+++ Sample/src/sample/Variable.java
@@ -3,14 +3,15 @@ package sample;
 public class Variable {
 
     public static void main(String[] args) {
+     var num = 1;
      
-        var number1 = 1;
-        System.out.println(number1);
-        
-        String str1= "abc";
-     //面白いと今は感じている
-        System.out.println(str1);
-
+     //ここでの「abc」は、記号ではなく文字列扱いになるので、必ず””をつける！
+     String str1= "abc" ;
+     
+     
+     System.out.println(num);
+     System.out.println(str1);
+     
     }
 
 }

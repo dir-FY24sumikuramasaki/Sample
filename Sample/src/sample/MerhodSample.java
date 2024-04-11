f8808ee1f9e1a9a946bc6d0d27dfda365761b290
dir-FY24sumikuramasaki <MasakiSumikura()@Dirbato-1302x>
2024-04-11 15:02:58 +0900

    終了

--- Sample/src/sample/MerhodSample.java
+++ Sample/src/sample/MerhodSample.java
@@ -7,8 +7,10 @@ public class MerhodSample {
     public static void main(String[] args) {
        var number1 = 100;
        var number2 = 10;
+       var number3 = 1;
        
        sumMethod1(number1,number2);
+       sumMethod1(number1,number3);
        
        var result2 = sumMethod2(number1,number2);
        System.out.println("sumMethod2の結果は"+ result2 +"です。");
